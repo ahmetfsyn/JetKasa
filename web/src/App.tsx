7c995605c7fcac2903d575d2630982b8775f4c07
Ahmet Furkan Sayan <21010310065@ogrenci.bartin.edu.tr>
2025-09-21 18:37:37 +0300

    toast message is added

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,16 @@
-function App() {
-  return <div></div>;
-}
+import { BrowserRouter } from "react-router";
+import AppRouter from "./routes/AppRouter";
+import { ToastContainer } from "react-toastify";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+      <ToastContainer />
+    </>
+  );
+};
 
 export default App;
