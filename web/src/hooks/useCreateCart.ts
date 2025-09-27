7755f318b02eebcca88c12ff867d51d588795ed4
Ahmet Furkan Sayan <21010310065@ogrenci.bartin.edu.tr>
2025-09-27 19:10:00 +0300

    axios config and cart service are added

---
+++ web/src/hooks/useCreateCart.ts
@@ -0,0 +1,28 @@
+import { createCartAsync } from "@/services/cartService";
+import useCartStore from "@/store/useCartStore";
+import { useMutation } from "@tanstack/react-query";
+
+export const useCreateCart = () => {
+  const { createCart: addCartToStore } = useCartStore();
+
+  const mutation = useMutation({
+    mutationFn: createCartAsync,
+  });
+
+  const handleCreateCart = async () => {
+    try {
+      const data = await mutation.mutateAsync();
+      console.log("data : ", data);
+      addCartToStore(data);
+      return data;
+    } catch (error) {
+      console.error("Cart oluşturulamadı:", error);
+      throw error;
+    }
+  };
+
+  return {
+    createCart: handleCreateCart,
+    ...mutation,
+  };
+};
