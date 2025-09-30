2b1ef8800f74c2e885b0ac296f57e6dc56548b6f
Ahmet Furkan Sayan <21010310065@ogrenci.bartin.edu.tr>
2025-09-30 14:35:21 +0300

    hotfix for prod

--- web/src/configs/axiosConfig.ts
+++ web/src/configs/axiosConfig.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const BASE_API_URL_DEV = "http://localhost:8080";
 export const BASE_API_URL_PROD =
   "https://resilient-tranquility-production.up.railway.app";
-export const IS_PROD = false;
+export const IS_PROD = true;
 
 export const api = axios.create({
   baseURL: IS_PROD ? BASE_API_URL_PROD : BASE_API_URL_DEV,
--- web/src/routes/AppRouter.tsx
+++ web/src/routes/AppRouter.tsx
@@ -1,8 +1,5 @@
 import { Route, Routes } from "react-router";
-import AuthRoutes from "./auth/AuthRoutes";
 import NotFound from "@/pages/NotFound";
-import CheckerRoutes from "./checker/CheckerRoutes";
-import PrivateRoute from "./PrivateRoute";
 import Welcome from "@/pages/Welcome";
 import MainLayout from "@/layouts/MainLayout";
 import CustomerRoutes from "./customer/CustomerRoutes";
@@ -12,15 +9,6 @@ import { CartGuard } from "@/layouts/CartGuard";
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/auth/*" element={<AuthRoutes />} />
-      <Route
-        path="/checker/*"
-        element={
-          <PrivateRoute>
-            <CheckerRoutes />
-          </PrivateRoute>
-        }
-      />
       <Route
         path="/customer/*"
         element={
